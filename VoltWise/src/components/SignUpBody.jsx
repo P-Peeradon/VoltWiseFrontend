216c2890bc604ea199af01b3198a6ca88b962444
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-11 12:52:54 +1000

    fix: padding y with responsiveness

--- VoltWise/src/components/SignUpBody.jsx
+++ VoltWise/src/components/SignUpBody.jsx
@@ -3,7 +3,7 @@ import SignUpForm from './SignUpForm'
 
 const SignUpBody = () => {
     return (
-        <main className=''>
+        <main className="flex items-center justify-center py-16 md:py-24 min-h-[calc(100vh-160px)]">
             <SignUpForm />
         </main>
     )
